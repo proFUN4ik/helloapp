251f393df32f317ba5bfd7bb1299c393e99420a6
white <white@example.com>
2021-11-03 21:18:05 +0500

    Add greetWhite

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,6 +4,11 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static void GreetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
+        
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
